281997eb24a0024654b3d36f8c044216bb639741
Sanjanaekanayake <sanjanagalle1@gmail.com>
2021-11-15 20:15:28 +0530

    fixed [env]

--- app/models/index.js
+++ app/models/index.js
@@ -1,42 +1,50 @@
 'use strict';
-
-const dbConfig = require("../../config/config.js");
+const fs = require('fs');
+const path = require('path');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = require(__dirname + '/../../config/config.js')["connectionString"];
+console.log(dbConfig);
 
 const Sequelize = require("sequelize");
 let sequelize;
 
-if (dbConfig.connectionString.use_env_variable) {
-  sequelize = new Sequelize(dbConfig.connectionString.use_env_variable,{
-    dialect: dbConfig.connectionString.dialect
-  });
+if (dbConfig.use_env_variable) {
+  sequelize = new Sequelize(process.env[dbConfig.use_env_variable],dbConfig);
 
 } else {
-  sequelize = new Sequelize(dbConfig.connectionString.DB, dbConfig.connectionString.USER, dbConfig.connectionString.PASSWORD, {
-    host: dbConfig.connectionString.HOST,
-    dialect: dbConfig.connectionString.dialect,
+  sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
     operatorsAliases: false,
     define: {
       timestamps: true,
       freezeTableName: true
     },
-    pool :dbConfig.connectionString.pool
+    pool :dbConfig.pool
   
   });
 }
-
-// const sequelize = new Sequelize(dbConfig.connectionString.DB, dbConfig.connectionString.USER, dbConfig.connectionString.PASSWORD, {
-//   host: dbConfig.connectionString.HOST,
-//   dialect: dbConfig.connectionString.dialect,
-//   operatorsAliases: false,
-//   define: {
-//     timestamps: true,
-//     freezeTableName: true
-//   },
-//   pool :dbConfig.connectionString.pool
-
-// });
-
 const db = {};
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+
+
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
